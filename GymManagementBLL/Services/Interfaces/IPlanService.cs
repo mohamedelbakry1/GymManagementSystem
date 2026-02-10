1d6d46ff13f7f6e68a472e5edf5ab9fa76e7a6c7
Mohamed Elbakary <albqrym58@gmail.com>
2026-02-10 14:15:29 +0200

    Implement Plan Service

---
+++ GymManagementBLL/Services/Interfaces/IPlanService.cs
@@ -0,0 +1,16 @@
+﻿using GymManagementBLL.ViewModels.PlanViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagementBLL.Services.Interfaces
+{
+    public interface IPlanService
+    {
+        IEnumerable<PlanViewModel> GetAllPlans();
+        PlanViewModel? GetPlanDetails(int PlanId);
+        UpdatePlanViewModel? GetPlanToUpdate(int PlanId);
+        bool UpdatePlanDetails(int PlanId, UpdatePlanViewModel updatePlan);
+        bool ToggleStatus(int PlanId);
+    }
+}
