441a51ccc856be7d526a020cb3432677bccc3c93
Mohamed Elbakary <albqrym58@gmail.com>
2026-02-10 17:33:47 +0200

    Implement Trainer Service

---
+++ GymManagementBLL/ViewModels/TrianerViewModels/TrainerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagementBLL.ViewModels.TrianerViewModels
+{
+    public class TrainerViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Specialities { get; set; } = null!;
+        public string? DateOfBirth { get; set; }
+        public string? Address { get; set; }
+    }
+}
---
+++ GymManagementBLL/ViewModels/TrianerViewModels/UpdateTrainerViewModel.cs
@@ -0,0 +1,35 @@
+﻿using GymManagementDAL.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GymManagementBLL.ViewModels.TrianerViewModels
+{
+    public class UpdateTrainerViewModel
+    {
+        public string Name { get; set; } = null!;
+        [Required(ErrorMessage = "Email is Required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Format")]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Email must be between 5 and 100 Chars")]
+        public string Email { get; set; } = null!;
+        [Required(ErrorMessage = "Phone is Required")]
+        [DataType(DataType.PhoneNumber)]
+        [Phone(ErrorMessage = "Invalid Phone Format")]
+        [RegularExpression(@"^(010|011|012|015\d{8}$)", ErrorMessage = "Phone Number must be valid Egyptian PhoneNumber")]
+        public string Phone { get; set; } = null!;
+        [Required(ErrorMessage = "Building Number is Required")]
+        [Range(1, 9000, ErrorMessage = "Building Number must be between 1 and 9000")]
+        public int BuildingNumber { get; set; }
+        [Required(ErrorMessage = "Street is Required")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Street must be between 2 and 30 Chars")]
+        public string Street { get; set; } = null!;
+        [Required(ErrorMessage = "City is Required")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "City must be between 2 and 30 Chars")]
+        [RegularExpression(@"[a-zA-Z\s]+$", ErrorMessage = "City can contain only letters and spaces")]
+        public string City { get; set; } = null!;
+        [Required(ErrorMessage = "Specialization is Required")]
+        public Specialties Specialties { get; set; }
+    }
+}
